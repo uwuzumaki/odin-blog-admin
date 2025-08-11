14177d56ff719502322a21c4e225a9af7b19663b
Kris Wong <krischwong@gmail.com>
2025-08-11 01:01:49 -0400

    added route for individual posts

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,19 @@
-import App from "./App";
+import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
+import Layout from "./Components/Layout/Layout";
+import PostPage from "./Components/PostPage";
 
 const routes = [
   {
     path: "/",
-    element: <App />,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      { path: "/post/:post_id", element: <PostPage /> },
+    ],
   },
   {
     path: "login",
