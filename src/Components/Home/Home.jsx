4fe12e909ce044e34e0bf2956612606566ceeb01
Kris Wong <krischwong@gmail.com>
2025-08-11 01:02:35 -0400

    Added home component

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const [allPosts, setAllPosts] = useState([]);
+
+  const getPosts = async () => {
+    const url = "http://localhost:3000/post";
+    try {
+      const posts = await axios.get(url);
+      setAllPosts(posts.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <p>Posts</p>
+        <div>
+          {allPosts.map((post) => (
+            <Link
+              to={`/post/${post.id}`}
+              className="border border-black"
+              key={post.id}
+            >
+              {post.title}
+              <p> {post.status ? "Displayed" : "Hidden"}</p>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
