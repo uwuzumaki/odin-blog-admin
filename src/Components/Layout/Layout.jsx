2624a602f5bac57f70f0b00250325211a87b3b1e
Kris Wong <krischwong@gmail.com>
2025-08-13 00:14:48 -0400

    made outlet expand

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,16 +1,17 @@
 import { Link, Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
 
 const Layout = () => {
   return (
-    <>
+    <div className="flex flex-1 flex-col">
       <nav>
         Nav <Link to="/login">Login</Link>
         <Link to="/Create">New Post</Link>
       </nav>
       <div>Layout</div>
       <Outlet />
-      <footer>footer</footer>
-    </>
+      <Footer />
+    </div>
   );
 };
 
