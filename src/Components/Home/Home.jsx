ae82498c7fe173995b80d395836b66142f381813
Kris Wong <krischwong@gmail.com>
2025-08-15 00:01:22 -0400

    styling for home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import moment from "moment";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -21,18 +22,28 @@ const Home = () => {
 
   return (
     <>
-      <div className="flex-1">
-        <p>Posts</p>
-        <div>
-          {allPosts.map((post) => (
-            <Link
-              to={`/post/${post.id}`}
-              className="border border-black"
+      <div className="flex flex-1 flex-col items-center justify-center">
+        <div className="flex w-[80%] flex-col gap-1 rounded-2xl border border-blue-400 bg-slate-50 shadow-blue-800">
+          <div className="my-2 flex justify-between border-b-1 border-blue-400">
+            <div className="flex-1 pl-4 text-2xl font-bold">Title</div>
+            <div className="flex flex-1 justify-between">
+              <div className="text-2xl font-bold">Created</div>
+              <div className="pr-4 text-2xl font-bold">Status</div>
+            </div>
+          </div>
+          {allPosts.map((post, index) => (
+            <div
               key={post.id}
+              className={`flex w-full justify-between py-2 ${index + 1 < allPosts.length ? `border-b-1 border-b-blue-400/30` : ``}`}
             >
-              {post.title}
-              <p> {post.status ? "Displayed" : "Hidden"}</p>
-            </Link>
+              <Link className="flex-1" to={`/post/${post.id}`}>
+                <p className="pl-4">{post.title}</p>
+              </Link>
+              <div className="flex flex-1 justify-between">
+                <p>{moment(post.createAt).format("MMMM Do, YYYY")}</p>
+                <p className="pr-4"> {post.status ? "Displayed" : "Hidden"}</p>
+              </div>
+            </div>
           ))}
         </div>
       </div>
