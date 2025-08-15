6395ab2f5974e9dc2fab5be8b09c280ba733736c
Kris Wong <krischwong@gmail.com>
2025-08-15 00:01:32 -0400

    navvbar styling

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,13 +2,23 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between">
+    <nav className="flex justify-between border-b border-b-slate-300 bg-slate-50 px-4 py-6">
       <div>
         <Link to="/">Blog</Link>
       </div>
       <div>
-        <Link to="/create">New Post</Link>
-        <Link to="/login">Login</Link>
+        <Link
+          className="m-2 rounded-4xl border border-blue-500 bg-blue-400 px-4 py-2 text-white shadow"
+          to="/create"
+        >
+          New Post
+        </Link>
+        <Link
+          className="m-2 rounded-4xl border px-4 py-2 text-blue-400"
+          to="/login"
+        >
+          Login
+        </Link>
       </div>
     </nav>
   );
