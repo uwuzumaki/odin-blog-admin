3f34c958c252956af9a761bc4dfef9f50e059033
Kris Wong <krischwong@gmail.com>
2025-08-19 22:31:59 -0400

    Added a loader for home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,14 +5,18 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   const [allPosts, setAllPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const getPosts = async () => {
     const url = "http://localhost:3000/post";
     try {
+      setLoading(true);
       const posts = await axios.get(url);
       setAllPosts(posts.data);
     } catch (err) {
       console.log(err);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -23,7 +27,7 @@ const Home = () => {
   return (
     <>
       <div className="flex flex-1 flex-col items-center justify-center">
-        <div className="flex w-[80%] flex-col gap-1 rounded-2xl border border-blue-400 bg-slate-50 shadow-blue-800">
+        <div className="flex min-h-[40vh] w-[80%] flex-col rounded-2xl border border-blue-400 bg-slate-50 shadow shadow-blue-800">
           <div className="my-2 flex justify-between border-b-1 border-blue-400">
             <div className="flex-1 pl-4 text-2xl font-bold">Title</div>
             <div className="flex flex-1 justify-between">
@@ -31,20 +35,32 @@ const Home = () => {
               <div className="pr-4 text-2xl font-bold">Status</div>
             </div>
           </div>
-          {allPosts.map((post, index) => (
-            <div
-              key={post.id}
-              className={`flex w-full justify-between py-2 ${index + 1 < allPosts.length ? `border-b-1 border-b-blue-400/30` : ``}`}
-            >
-              <Link className="flex-1" to={`/post/${post.id}`}>
-                <p className="pl-4">{post.title}</p>
-              </Link>
-              <div className="flex flex-1 justify-between">
-                <p>{moment(post.createAt).format("MMMM Do, YYYY")}</p>
-                <p className="pr-4"> {post.status ? "Displayed" : "Hidden"}</p>
-              </div>
+          {loading ? (
+            <div className="flex flex-1 items-center justify-center">
+              <div class="mr-2 ml-1 h-4 w-4 animate-spin rounded-full border-2 border-blue-400 border-t-transparent"></div>
+              <p className="animate-pulse">Loading...</p>
             </div>
-          ))}
+          ) : (
+            <>
+              {allPosts.map((post, index) => (
+                <div
+                  key={post.id}
+                  className={`flex w-full justify-between py-2 ${index + 1 < allPosts.length ? `border-b-1 border-b-blue-400/30` : ``}`}
+                >
+                  <Link className="flex-1" to={`/post/${post.id}`}>
+                    <p className="pl-4">{post.title}</p>
+                  </Link>
+                  <div className="flex flex-1 justify-between">
+                    <p>{moment(post.createAt).format("MMMM Do, YYYY")}</p>
+                    <p className="pr-4">
+                      {" "}
+                      {post.status ? "Displayed" : "Hidden"}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </>
+          )}
         </div>
       </div>
     </>
