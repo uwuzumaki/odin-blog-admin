c0af40b84f7519d741242d40077079048787c8c7
Kris Wong <krischwong@gmail.com>
2025-08-24 22:08:10 -0400

    added loaders to routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,21 +3,33 @@ import Login from "./Components/Login/Login";
 import Layout from "./Components/Layout/Layout";
 import PostPage from "./Components/Postpage/PostPage";
 import Create from "./Components/Create/Create";
+import authLoader from "./Loaders/authLoader";
+import protectedLoader from "./Loaders/protectedLoader";
 
 const routes = [
   {
     path: "/",
     element: <Layout />,
+    loader: authLoader,
+    id: "root",
     children: [
       {
         index: true,
         element: <Home />,
       },
-      { path: "/post/:post_id", element: <PostPage /> },
-      { path: "/create", element: <Create /> },
+      {
+        path: "/post/:post_id",
+        loader: protectedLoader,
+        element: <PostPage />,
+      },
+      { path: "/create", loader: protectedLoader, element: <Create /> },
       {
         path: "login",
         element: <Login />,
+      },
+      {
+        path: "/login/verify",
+        element: <authLoader />,
       },
     ],
   },
