552b5ffb5fab8dcaaa1cd8f22dbd84d4050e1348
Kris Wong <krischwong@gmail.com>
2025-08-24 22:36:30 -0400

    styling for login page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,9 +2,10 @@ import axios from "axios";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../Contexts/AuthContext";
+import { useEffect } from "react";
 
 const Login = () => {
-  const { setUser } = useAuth();
+  const { setUser, user } = useAuth();
   const {
     register,
     handleSubmit,
@@ -25,14 +26,34 @@ const Login = () => {
     reset();
   };
 
+  useEffect(() => {
+    user ? navigate("/") : null;
+    //eslint-disable-next-line
+  }, [user]);
+
   return (
     <div className="flex flex-1 flex-col items-center justify-center">
-      <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
-        <label>Username</label>
-        <input {...register("username")} />
-        <label>Password</label>
-        <input type="password" {...register("password")} />
-        <input type="submit" value="Submit" />
+      <form
+        className="flex flex-col items-center justify-center rounded-xl border border-blue-500 p-6"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <input
+          className="m-4 border-b-1 transition duration-500 ease-in-out focus:border-blue-500 focus:outline-none"
+          autoComplete="off"
+          placeholder="Username"
+          {...register("username")}
+        />
+        <input
+          className="m-4 border-b-1 transition duration-500 ease-in-out focus:border-blue-500 focus:outline-none"
+          placeholder="Password"
+          type="password"
+          {...register("password")}
+        />
+        <input
+          className="m-2 w-fit cursor-pointer rounded-4xl border border-blue-500 bg-blue-400 px-4 py-2 text-white shadow"
+          type="submit"
+          value="Submit"
+        />
       </form>
     </div>
   );
