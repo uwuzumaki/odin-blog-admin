cd8cc8c8590eb14546216605a75912ab2ba6acfe
Kris Wong <krischwong@gmail.com>
2025-08-25 02:33:33 -0400

    Added back button

--- src/Components/Postpage/PostPage.jsx
+++ src/Components/Postpage/PostPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
 
 const PostPage = () => {
@@ -43,41 +43,53 @@ const PostPage = () => {
   }, []);
 
   return (
-    <div className="flex h-1/1 flex-1 flex-col items-center justify-center">
-      <div className="flex max-h-[40vh] min-h-[40vh] max-w-[40%] min-w-[40%] flex-col rounded-2xl border-blue-400 bg-slate-50 shadow shadow-blue-800">
-        {loadingPost ? (
-          <div className="flex flex-1 items-center justify-center">
-            <p className="animate-pulse">Loading Post...</p>
-          </div>
-        ) : (
-          <>
-            <p className="border-b-1 border-blue-400 px-4 py-2">{post.title}</p>
-            <p className="flex-1 overflow-y-auto px-4 py-4">{post.content}</p>
-          </>
-        )}
-        <div className="flex items-center justify-between justify-self-end border-t-1 border-blue-400 pt-4 pb-2">
-          <p className="px-4"> Status: {visibility ? "Published" : "Hidden"}</p>
-          {updating ? (
-            <div className="mx-4 my-2 flex cursor-not-allowed items-center justify-center rounded border border-blue-500 bg-blue-400 px-2 py-1 font-bold">
-              {" "}
-              <div className="mr-2 ml-1 h-4 w-4 animate-spin rounded-full border-2 border-white border-t-transparent"></div>
-              {visibility ? (
-                <p className="font-bold text-white">Hiding...</p>
-              ) : (
-                <p className="font-bold text-white">Publishing...</p>
-              )}
+    <>
+      <div className="py-4">
+        <Link className="m-4" to="/">
+          &lsaquo;-Back
+        </Link>
+      </div>
+      <div className="flex h-1/1 flex-1 flex-col items-center justify-center">
+        <div className="flex max-h-[40vh] min-h-[40vh] max-w-[40%] min-w-[40%] flex-col rounded-2xl border-blue-400 bg-slate-50 shadow shadow-blue-800">
+          {loadingPost ? (
+            <div className="flex flex-1 items-center justify-center">
+              <p className="animate-pulse">Loading Post...</p>
             </div>
           ) : (
-            <div
-              className="mx-4 my-2 max-w-full cursor-pointer self-start rounded border border-blue-500 bg-blue-400 px-2 py-1 font-bold text-white"
-              onClick={() => toggleVis(location.pathname.split("/")[2])}
-            >
-              {visibility ? "Hide" : "Publish"}
-            </div>
+            <>
+              <p className="border-b-1 border-blue-400 px-4 py-2">
+                {post.title}
+              </p>
+              <p className="flex-1 overflow-y-auto px-4 py-4">{post.content}</p>
+            </>
           )}
+          <div className="flex items-center justify-between justify-self-end border-t-1 border-blue-400 pt-4 pb-2">
+            <p className="px-4">
+              {" "}
+              Status: {visibility ? "Published" : "Hidden"}
+            </p>
+            {updating ? (
+              <div className="mx-4 my-2 flex cursor-not-allowed items-center justify-center rounded border border-blue-500 bg-blue-400 px-2 py-1 font-bold">
+                {" "}
+                <div className="mr-2 ml-1 h-4 w-4 animate-spin rounded-full border-2 border-white border-t-transparent"></div>
+                {visibility ? (
+                  <p className="font-bold text-white">Hiding...</p>
+                ) : (
+                  <p className="font-bold text-white">Publishing...</p>
+                )}
+              </div>
+            ) : (
+              <div
+                className="mx-4 my-2 max-w-full cursor-pointer self-start rounded border border-blue-500 bg-blue-400 px-2 py-1 font-bold text-white"
+                onClick={() => toggleVis(location.pathname.split("/")[2])}
+              >
+                {visibility ? "Hide" : "Publish"}
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
