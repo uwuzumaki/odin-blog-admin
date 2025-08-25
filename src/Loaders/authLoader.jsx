f641998964cf8e0a96b21837da56ce9a44f501d6
Kris Wong <krischwong@gmail.com>
2025-08-24 20:30:18 -0400

    Added loaders for routes

---
+++ src/Loaders/authLoader.jsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const authLoader = async () => {
+  const url = "http://localhost:3000/login/verify";
+  try {
+    const res = await axios.get(url, { withCredentials: true });
+    if (res.statusText == "OK") {
+      const user = res.data.user;
+      return { user, authenticated: true };
+    }
+    return { user: null, authenticated: false };
+  } catch (err) {
+    console.log(err);
+    return { user: null, authenticated: false };
+  }
+};
+
+export default authLoader;
---
+++ src/Loaders/protectedLoader.jsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { redirect } from "react-router-dom";
+
+const protectedLoader = async () => {
+  const url = "http://localhost:3000/login/verify";
+  try {
+    const res = await axios.get(url, { withCredentials: true });
+    if (res.statusText != "OK") {
+      return redirect("/login");
+    }
+    const user = res.data.user;
+    return { user };
+  } catch (err) {
+    console.log(err);
+    return redirect("/login");
+  }
+};
+
+export default protectedLoader;
