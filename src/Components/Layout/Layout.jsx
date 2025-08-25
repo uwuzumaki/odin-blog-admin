1de90188f3f23ae0d0e71c5d4a870f8b83429616
Kris Wong <krischwong@gmail.com>
2025-08-24 20:27:13 -0400

    added authprovider and loader implementation

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,13 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useRouteLoaderData } from "react-router-dom";
+import { AuthProvider } from "../../Contexts/AuthContext";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 
 const Layout = () => {
+  const { user } = useRouteLoaderData("root");
   return (
     <div className="flex flex-1 flex-col">
-      <Navbar />
-      <Outlet />
-      <Footer />
+      <AuthProvider initialUser={user}>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </AuthProvider>
     </div>
   );
 };
